eb67c2cac21015b450fc0f6dd74a39a2e81d409f
Luke Sheppard <sheppiegit@gmail.com>
2024-06-22 17:41:47 -0400

    Added the used literal values in Token, added a constructor, updated the token EOF to END to reduce naming conflicts with C++ language, created  a type alias for the Token Literals used from c++.  Changed the name of the file to Token.hpp from Token.h to reduce clang warnings.

---
+++ Token.hpp
@@ -0,0 +1,73 @@
+#pragma once
+#include <string>
+#include <variant>
+
+namespace lox
+{
+    enum TokenType
+    {
+        //Single-char tokens
+        LEFT_PAREN,
+        RIGHT_PAREN,
+        LEFT_BRACE,
+        RIGHT_BRACE,
+        COMMA,
+        DOT,
+        MINUS,
+        PLUS,
+        SEMICOLON,
+        SLASH,
+        STAR,
+
+        //one, or two char tokens
+        BANG,
+        BANG_EQUAL,
+        EQUAL,
+        EQUAL_EQUAL,
+        GREATER,
+        GREATER_EQUAL,
+        LESS,
+        LESS_EQUAL,
+
+        //literal
+        IDENTIFIER,
+        STRING,
+        NUMBER,
+
+        //keywords,
+        AND,
+        CLASS,
+        ELSE,
+        FALSE,
+        FUN,
+        FOR,
+        IF,
+        NIL,
+        OR,
+        PRINT,
+        RETURN,
+        SUPER,
+        THIS,
+        TRUE,
+        VAR,
+        WHILE,
+
+        END
+    };
+
+    using TokenLiteral = std::variant<std::string_view, double, bool, std::nullptr_t>;
+
+    class Token
+    {
+    private:
+        TokenType type;
+        std::string_view lexeme;
+        TokenLiteral literal;
+        unsigned int line;
+    public:
+        explicit Token(const TokenType& type, const std::string_view& lexeme, const TokenLiteral& literal, const unsigned int& line)
+        : type{type}, lexeme{lexeme}, literal{literal}, line{line}
+        {}
+    };
+
+}
