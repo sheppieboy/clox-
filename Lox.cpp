829838fa4b2faecf932b2c3e5cb368aaea81efc2
Luke Sheppard <sheppiegit@gmail.com>
2024-06-22 14:42:36 -0400

    read from file and print. Does not cover error handling at all.

--- Lox.cpp
+++ Lox.cpp
@@ -1,10 +1,23 @@
+#include <fstream>
+#include <sstream>
 #include <iostream>
+
+void readFromFile(const std::string& path)
+{
+    const std::ifstream input_stream(path, std::ios::binary);
+    std::stringstream buffer;
+    buffer << input_stream.rdbuf();
+
+    std::cout << buffer.str() << '\n';
+}
 
 int main(int argc, char *argv[])
 {
     std::cout << argc << " files pasted" << "\n";
     std::cout << argv[1] << " : is a file, and was passed\n";
     std::cout << argv[0] << " : executeable passed\n";
+
+    readFromFile(argv[1]);
 
     return 0;
 }
