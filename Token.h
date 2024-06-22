d9111aa0c4af2de88dc3fa34930cd60f0bb6b105
Luke Sheppard <sheppiegit@gmail.com>
2024-06-22 15:45:36 -0400

    enum containing token type.

---
+++ Token.h
@@ -0,0 +1,52 @@
+#pragma once
+
+enum TokeType
+{
+    //Single-char tokens
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    //one, or two char tokens
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    //literal
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    //keywords,
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    EOF
+}
